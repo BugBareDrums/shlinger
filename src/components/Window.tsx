16c3f3f79f5ab23a76602a29af2afdf10480dce6
JamesManningR <JamesManningR98@gmail.com>
2023-12-19 16:19:12 +0000

    added all window components

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -3,6 +3,33 @@ type WindowProps = {
   className?: string;
 } & React.ButtonHTMLAttributes<HTMLDivElement>;
 
-export const Window = ({ children, className }: WindowProps) => {
-  return <div className={`bg-white p-4 ${className}`}>{children}</div>;
+type Stages = "accuse" | "accused" | "defending" | "truth" | "lie";
+
+const imgs: Record<Stages, string> = {
+  accuse: "/accuse.png",
+  accused: "/accused.png",
+  defending: "/defending.png",
+  truth: "/truth.png",
+  lie: "/lie.png",
 };
+
+export const Img = ({ stage }: { stage: Stages }) => {
+  return <img className="mb-2" src={imgs[stage]} alt={stage} />;
+};
+
+const Title = ({ children }: { children: React.ReactNode }) => {
+  return <h2 className="text-lg font-bold">{children}</h2>;
+};
+const Subtitle = ({ children }: { children: React.ReactNode }) => {
+  return <p>{children}</p>;
+};
+
+export const Window = ({ children, className }: WindowProps) => {
+  return (
+    <div className={`text-center bg-white p-4 ${className}`}>{children}</div>
+  );
+};
+
+Window.Title = Title;
+Window.Subtitle = Subtitle;
+Window.Img = Img;
