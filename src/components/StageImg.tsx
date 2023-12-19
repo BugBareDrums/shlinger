6de536740112607dbbc018ef159b589daf72b7b8
JamesManningR <JamesManningR98@gmail.com>
2023-12-19 16:12:29 +0000

    added image map

---
+++ src/components/StageImg.tsx
@@ -0,0 +1,9 @@
+type Stages = "accuse" | "accused" | "defending" | "truth" | "lie";
+
+const imgs: Record<Stages, string> = {
+  accuse: "/accuse.png",
+  accused: "/accused.png",
+  defending: "/defending.png",
+  truth: "/truth.png",
+  lie: "/lie.png",
+};
