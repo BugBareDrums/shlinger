0c1cd68708723ba54bf418064bca01f87a57f6c8
Nick Froud <nick.froud@ebay.com>
2023-12-22 14:31:35 +0000

    typos

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import {Window} from "../components/Window";
+import { Window } from "../components/Window";
 
 export const Welcome = () => {
     return (
@@ -7,11 +7,11 @@ export const Welcome = () => {
             <Window.Img stage="welcome" />
             <Window.Title>welcome to shlinger</Window.Title>
             <div className="text-left">
-            <p>this game is all about spreading the festive cheer and letting santa know whose been good and whose been bad amongst your fellow shlingers</p>
+            <p>this game is all about spreading the festive cheer and letting santa know who's been good and who's been bad amongst your fellow shlingers</p>
             <br/>
             <p className="text-sm">the options you have are:</p>
             <ul className="text-sm list-disc list-inside ml-2">
-                <li>make a CLAIM about someone; chose who, tell all, and set the parameters for it's success or failure</li>
+                <li>make a CLAIM about someone; chose who, tell all</li>
                 <li>if enough corroborators back you up it becomes a TRUTH</li>
                 <li>if too many people call your claim out it could become a LIE</li>
             </ul>
@@ -21,7 +21,7 @@ export const Welcome = () => {
             </p>
             <ul className="text-sm list-disc list-inside ml-2">
                 <li>refute claims that get thrown your away if you want santa to know you've been good</li>
-                <li>jump on others claims to back their claims up and ensure santa knows whose been bad</li>
+                <li>jump on others claims to back their claims up and ensure santa knows who's been bad</li>
             </ul>
             <br/>
             <p className="text-sm">once the game ends:</p>
